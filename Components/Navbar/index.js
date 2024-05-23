dcdce5116c60ed481083e89a718aaf4819794ddf
Celine_00 <cwang325@my.bcit.ca>
2024-05-23 16:58:34 -0700

    navbar done

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -10,11 +10,11 @@ const Navbar = () => {
     <div className={styles.navContainer}>
       <nav className={styles.navbar}>
         <div className={styles.navItem} onClick={() => router.push('/home')}>
-          <Image src="/images/home_icon.svg" alt="Home" width={55} height={55} />
+          <Image src="/images/home_icon.svg" alt="Home" width={60} height={60} />
         </div>
         <div className={styles.addButtonContainer} onClick={() => router.push('/add')}>
           <div className={styles.addButton}>
-            <Image src="/images/add_icon.svg" alt="Add" width={66.67} height={66.67} />
+            <Image src="/images/add_icon.svg" alt="Add" width={80} height={80} />
           </div>
         </div>
         <div className={styles.navItem} onClick={() => router.push('/recipes')}>
